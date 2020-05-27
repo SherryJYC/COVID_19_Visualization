ddfe23869960d91a3d75dd2fc5c62b0326b30757
Sherry_Jiang <sherry@student-net-vpn-3249.ethz.ch>
2020-05-27 14:03:39 +0800

    add timeline

--- covid19-web-app/src/components/China.vue
+++ covid19-web-app/src/components/China.vue
@@ -9,10 +9,32 @@
 
       <v-row>
         <v-col>
-         <h2>show deck</h2>
+         <h2>China COVID Map</h2>
          <div class="deck-container">
           <div id="china-map" ref="map"></div>
-          <canvas id="deck-canvas" ref="canvas"></canvas>
+          
+          <div>
+            <!-- <h4>Timeline</h4> -->
+            <label id="month"></label>
+            <b-btn class="play">
+              <BIconPlayFill id="playicon"/>
+              <BIconPauseFill id="stopicon"/>
+            </b-btn>
+            
+            <input class="slider" type="range" min="1" max="95" step="1" value="1" />
+          </div>
+        </div>
+
+        <div class='legend-container'>
+        <div class='legend' id='legend' >
+            <h2 class="legend">Confirmed</h2> 
+            <hr/>
+            
+            <!-- Div where the dynamic legend is created  -->	
+            <div class='legend' id='cd-legend' >
+            </div>
+        
+        </div>
         </div>
 
 
@@ -38,11 +60,11 @@
 
 <script>
 import { mapboxgl } from "@/main";
-// import {HexagonLayer} from '@deck.gl/aggregation-layers';
-// import {MapboxLayer} from '@deck.gl/mapbox';
-// import * as d3 from "d3";
+import {dates} from "../assets/json/dates"
+import JQuery from 'jquery';
 
-// var csv2geojson = require('csv2geojson');
+let $ = JQuery;
+var playSpeed = 200;
 
 export default {
   name: "China",
@@ -55,15 +77,24 @@ export default {
     ],
     token:
     "pk.eyJ1Ijoic2hlcnJ5anljIiwiYSI6ImNrYWhuNnUyaDBpMW8yeHQ5YmU5bjRxbmYifQ.rTKiRvlmkUa2IfJl9ToD9g",
-    csv_url: // trial of deck.gl example
-    'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv',
-    covid_url: // data of COVID19 updated in 7 May
-    'data/DXYcity0507.csv',
-    data_url:'mapbox://sherryjyc.7xgdtkm5'
+    // csv_url: // trial of deck.gl example
+    // 'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv',
+    // covid_url: // data of COVID19 updated in 7 May
+    // 'data/DXYcity0507.csv',
+    data_url:'mapbox://sherryjyc.7xgdtkm5',
+    dates,
+    breaks: [
+      [10,"#007A96"],
+      [50, "#72791C"],
+      [200, "#485A2C"],
+      [ 1000,"#956013"],
+       [10000,"#A8322D"],
+    ]
   }),
   created() {
     this.map = null;
-    // this.deck = null;
+    this.animation = null;
+    this.play = true;
   },
   methods: {
     initMap: function() {
@@ -75,14 +106,48 @@ export default {
             minZoom:4,
             maxZoom:10,
             zoom: 4,
-            pitch: 40.5,
+            pitch: 45,
             antialias: true
         });
+    },
+    filterBy: function(date) {
+      var filters = ['==', 'Date', date];
+      this.map.setFilter('covid', filters);
+      
+      // TODO: Set the label 
+      
+    },
+    autoPlay: function(playSpeed){
+      this.animation = setInterval(this.moveTime,playSpeed);
+
+    },
+    moveTime: function(){
+      var currentDate = parseInt($('.slider').val());   
+      currentDate+=1;
+      // if already at latest date, start from beginning 
+      if (currentDate == 96){
+        currentDate = 1;
+      }  
+      $('.slider').val(currentDate);
+      $('.slider').trigger('change');
     }
+
 
   },
   mounted() {
         this.initMap();
+
+        var legendLabels = [
+                        // Label text that will appear in the legend [0]
+                                ['< 50'],
+                                ['50 - 200'],
+                                ['200 - 1000'],
+                                ['1000 - 10000'],
+                                ['> 10000'],
+            ];	
+        var breaksRev = this.breaks.slice().reverse();
+        var legendLabelsRev = legendLabels.slice().reverse();
+        var legend = document.getElementById('cd-legend'); 
 
         this.map.on('style.load', () => {
 
@@ -95,7 +160,7 @@ export default {
               'id': 'covid',
               'source': 'dxy0507',
               'source-layer': 'DXY0507_Polygon5-3e9elo',
-              'filter': ['==', 'Date', '04-27'], // 04-27 for wuhan
+              // 'filter': ['==', 'Date', '01-27'], // 04-27 for wuhan
               'type': 'fill-extrusion',
               'minzoom': 4,
               'paint': {
@@ -128,31 +193,61 @@ export default {
               'fill-extrusion-base': 0,
               'fill-extrusion-opacity': 0.6
               }
-            });      
+            });     
+            // set filter
+            this.filterBy(this.dates[0]); 
+            // help call 'this' when out of scope
+            var ref = this;
 
-            // hexagonLayer = new MapboxLayer({
-            //     type: HexagonLayer,
-            //     id: 'covid',
-            //     data: d3.csv(this.covid_url),
-            //     radius: 1000,
-            //     coverage: 1,
-            //     upperPercentile: 100,
-            //     colorRange: this.COLOR_RANGE,
-            //     // elevationRange: [0, 1000],
-            //     elevationScale: 1000,
-            //     extruded: true,
-            //     getPosition: d => [Number(d.Longitude), Number(d.Latitude)],
-            //     getElevation: d => {
-            //         return Number(d.city_confirmedCount);
-            //     },
-            //     lightSettings: this.LIGHT_SETTINGS,
-            //     opacity: 1
-            // });
-            // Add the deck.gl hex layer below labels in the Mapbox map
-            // this.map.addLayer(hexagonLayer, 'waterway-label');
-            // var filters = ['==', 'cityEnglishName', "Chongqing"];
-            // this.map.setFilter('covid',filters);
+            $('.slider').change(function(e) {
+                var date = parseInt(e.target.value)-1;
+                ref.filterBy(ref.dates[date]);
+            });
+            // set auto play of slider
+
+            var stopicon = document.getElementById("stopicon")
+            var playicon = document.getElementById("playicon");
+            
+            $('.play').click(function () {
+              if (ref.play == true){
+                  ref.autoPlay(playSpeed);
+                  ref.play = false;
+                  stopicon.style.visibility = "visible";
+                  playicon.style.visibility = "hidden";
+              }
+              else {
+                clearInterval(ref.animation);
+                ref.play = true;
+                stopicon.style.visibility = "hidden";
+                playicon.style.visibility = "visible";
+              }
+          });
+          breaksRev.forEach(function(layer, i){
+              var item = document.createElement('div');
+              var key = document.createElement('span');
+              var value = document.createElement('span');
+                            
+              key.style.backgroundColor = layer[1]; 
+              key.style.width = "50px";
+              key.style.display = "inline-block";
+              key.style.margin = "10px";
+              key.textContent = "|";
+
+              // give the value variable a placeholder id that we can access and update with custom labels
+              value.id = 'legend-value-' + i;
+                          
+              item.appendChild(key); 
+              item.appendChild(value);
+              legend.appendChild(item);        
+          });
+                
+          legendLabelsRev.forEach(function(layer, i){
+            //as we iterate through the arrays get the correct row, and add the appropriate text 
+            document.getElementById('legend-value-' + i).textContent = layer[0];             
+           });  
+
         });
+
         
     }
 };
@@ -162,4 +257,77 @@ export default {
 #china-map {
   height: 600px;
 }
+#stopicon{
+  visibility:hidden;
+}
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 15px;
+  border-radius: 5px;  
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%; 
+  background: #A8322D;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #4CAF50;
+  cursor: pointer;
+}
+/* Mapbox Legend */
+    .legend {
+        font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
+        background-color: white; 
+        padding: 10px;
+        opacity: 0.8;
+    }
+
+    #cd-legend {
+        text-align: left;
+    }
+    
+    /* legend test */
+    .legend-container {
+        position: absolute;
+        margin: 5px;
+        top: 60px;
+        left: 10px;
+        padding: 0px 10px;
+        margin-bottom: 30px;
+        z-index: 1;
+        max-width: 300px
+    }
+    
+    /* Legend title */
+    .legend h2 {
+        padding-top: 8px;
+        margin:0;
+        text-align: center;
+    }
+
+    .legend hr {
+        color: #123455; 
+        border: 0; 
+        border-top: 1px solid #ccc; 
+        padding: 0; 
+        margin-top:5px;
+        margin-bottom:5px
+    }
+        
+    
 </style>
--- covid19-web-app/src/main.js
+++ covid19-web-app/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import { BootstrapVue, IconsPlugin, BIconPlayFill, BIconPauseFill } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.component('BIconPlayFill',BIconPlayFill)
+Vue.component('BIconPauseFill',BIconPauseFill)
 
 export const mapboxgl = window.mapboxgl;
 
