546d33b7a38947ebd2f97cd5255806b4d4651486
Sherry_Jiang <sherry@student-net-vpn-0455.ethz.ch>
2020-06-03 13:14:57 +0800

    add comparison

--- covid19-web-app/src/components/Comparison.vue
+++ covid19-web-app/src/components/Comparison.vue
@@ -8,12 +8,128 @@
                 Compare them based on disease and actions...
                 <b>Placeholder for Text</b>
             </b-card>
+
+            <!-- Comparison -->
+            <b-card-group deck>
+                <b-card class="black-content">
+                <div class="align-middle"><b>Comparison of Cumulative Confirmed, Recovered and Dead Count</b></div>
+                    <apexchart class="apexchart" type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
+                </b-card>
+                <!-- Description of Comparison-->
+                <b-card class="black-content">
+                <div class="align-middle"><b>Comparison of Death Rate</b></div>
+                <v-row>
+                    <v-col class="align-middle">COVID-19 in China <h3><b-badge variant="danger">5.50%</b-badge></h3></v-col>
+                    <v-col class="align-middle">SARS in China <h3> <b-badge variant="danger">9.56%</b-badge></h3></v-col>
+                </v-row>
+                Although more people infected by COVID-19, the death rate is lower that SARS...
+                <b>Placeholder for text</b>
+                </b-card>
+            </b-card-group>
+
+            <!-- Discussion -->
+            <b-card-group deck>
+                <b-card class="black-content">
+                    <h3 class="display-2 font-weight-bold mb-3">Diseases</h3>
+                    Characteristics of COVID-19:
+                    <br/>
+                    Characteristics of SARS:
+                </b-card>
+                <b-card class="black-content">
+                    <h3 class="display-2 font-weight-bold mb-3">Government Actions</h3>
+                    Difference in government actions:
+                    <br/>
+                    The blockdown of Wuhan in COVID-19 case...
+                <b>Placeholder for text</b>
+                </b-card>
+            </b-card-group>
+
       </v-container>
     </div>
 </template>
 
 <script>
+import VueApexCharts from 'vue-apexcharts';
+
 export default {
+    components:{
+        apexchart: VueApexCharts,
+    },
+    data: () => ({
+          series: [{
+            name: 'COVID-19',
+            data: [84402, 79077, 4643]
+          }, {
+            name: 'SARS',
+            data: [8096, 7000, 774]
+          }],
+          chartOptions: {
+            chart: {
+              type: 'bar',
+              height: 350,
+               toolbar: {
+                show: false,
+                },
+            },
+            plotOptions: {
+              bar: {
+                horizontal: false,
+                columnWidth: '55%',
+              },
+            },
+            dataLabels: {
+              enabled: true,
+              style: {
+                    colors: ['#FFFFFF', '#FFFFFF', '#FFFFFF']
+                },
+                offsetY: 20,
+            },
+            stroke: {
+              show: true,
+              width: 1,
+              colors: ['transparent']
+            },
+            xaxis: {
+              categories: ['Confirmed','Recovered', 'Dead'],
+              labels:{
+                  style:{
+                      colors: ['#FFFFFF', '#FFFFFF', '#FFFFFF']
+                  }
+
+              }
+            },
+            yaxis: {
+              title: {
+                text: 'people',
+                style:{
+                      color: '#FFFFFF', 
+                }
+              },
+              labels:{
+                  style:{
+                      colors: ['#FFFFFF', '#FFFFFF', '#FFFFFF','#FFFFFF', '#FFFFFF', '#FFFFFF']
+                  }
+              }
+       
+            },
+            legend:{
+                labels:{
+                    colors: '#FFFFFF',
+              }
+            },
+            fill: {
+              opacity: 1
+            },
+            tooltip: {
+              y: {
+                formatter: function (val) {
+                  return val + " people"
+                }
+              }
+            }
+          }
+          
+    }),
 
 }
 </script>
@@ -22,6 +138,11 @@ export default {
 .black-content {
   background-color: #121212;
 }
-
+.apexchart{
+    color:black;
+}
+.align-middle{
+    text-align: center;
+}
 </style>>
 
